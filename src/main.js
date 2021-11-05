e4bc8c8073079eac14e1051f421e78c76c06b56d
joaquinlmartin <joaquinlmartin@gmail.com>
2021-11-05 19:23:33 +0100

    splashscreen working

--- src/main.js
+++ src/main.js
@@ -1,29 +1,43 @@
 // splashScreen
-
-document.addEventListener('load', () => { 
+console.log("fuera");
+document.addEventListener('DOMContentLoaded', () => { 
+  console.log("load");
 let game;
+//let splashScreen;
+
+function printGameOver() {
+  let gameOver = document.getElementById('gameover');
+  let canvas = document.querySelector('#nemesis');
+  canvas.style = 'display: none';
+  gameOver.style = 'display: block';
+}
 
 
 function createSplashScreen() {
-  const main = document.getElementById('menu');
-  const enter = document.getElementById('enter');
 
-  const enterButton = document.querySelector('#enter');
-  enterButton.addEventListener('click', () => {
+ //const start = document.getElementById('start');
+
+  const startButton = document.querySelector('#start');
+  startButton.addEventListener('click', () => {
+    console.log("hola");
     menu.classList.add('hide');
     const game = document.getElementById('game');
     game.classList.remove('hide');
     game.classList.add('show');
+    let canvas = game.querySelector('#nemesis');
+    const ctx = canvas.getContext('2d');
+    const snakeGame = new Game(
+      {
+        ctx: ctx,
+        rows: canvas.width / 10,
+        columns: canvas.height / 10,
+        maxCells: 10,
+        nemesis: new Nemesis(canvas.width / 10, canvas.height / 10),
+      },
+      printGameOver
+    );
 
-    const startButton = game.querySelector('#start');
-    startButton.addEventListener('click', () => {
-      let canvas = game.querySelector('#nemesis');
-      const ctx = canvas.getContext('2d');
-      
-
-      
-      
-    })
+    nemesisGame.start();
 
   });
 }
